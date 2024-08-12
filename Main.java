de4a5eaef4e6274712bd4753c3c5aa79cc2bb347
Russkiyoki <elivoich@gmail.com>
2024-08-11 17:48:10 -0700

    LayeredPanels are even in size and all fit, Organized code into methods for easy reading. ToDo: add button, comments for documentation.

--- Main.java
+++ Main.java
@@ -1,33 +1,86 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 public class Main {
- 
+
+
   public static void main(String[] args) { 
     //random essentials
-    
-
-
     //Load up from base to top layer order
     JFrame frame = new JFrame("Gym Progress");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(new Dimension(1000, 750));
     frame.setLayout(null);
     frame.setVisible(true);
+    frame.setResizable(false);
 
-    JPanel panel = new JPanel();
-    panel.setBounds(0,0,1000,750);
-    panel.setBackground(new Color(45,45,50));
+    Insets insets = frame.getInsets();
+    int frameWidth = 1000;
+    int frameHeight = 750;
 
-    JLayeredPane layeredPane = new JLayeredPane();
-    layeredPane.setBounds(0,0,500,500);
-    
+    int panelWidth = frameWidth - (insets.left + insets.right);
+    int panelHeight = frameHeight - (insets.top + insets.bottom);
 
+    JPanel panel = createPanel();
+
+    JLayeredPane TL = createJLayeredPaneTopLeft(panelWidth, panelHeight);
+    JLayeredPane TR = createJLayeredPaneTopRight(panelWidth, panelHeight);
+    JLayeredPane BL = createJLayeredPaneBottomLeft(panelWidth, panelHeight);
+    JLayeredPane BR = createJLayeredPaneBottomRight(panelWidth, panelHeight);
 
     frame.add(panel);
-    panel.add(layeredPane);
-
+    panel.add(TL);
+    panel.add(TR);
+    panel.add(BL);
+    panel.add(BR);
 
   }
 
+  private static JPanel createPanel()
+  {
+    JPanel p = new JPanel(null);
+    p.setBounds(0,0,1000,750);
+    p.setBackground(new Color(45,45,50));
+    return p;
+
+  } 
+
+  private static JLayeredPane createJLayeredPaneTopLeft(int pW, int pH)
+  {
+    //pW = panelWidth
+    //pH = panelHeight
+    JLayeredPane layeredPaneTopLeft = new JLayeredPane();
+    layeredPaneTopLeft.setBounds(0,0,pW/2,pH/2);
+    layeredPaneTopLeft.setBorder(new LineBorder(Color.red));
+
+    return layeredPaneTopLeft;
+  }
+
+  private static JLayeredPane createJLayeredPaneTopRight(int pW, int pH)
+  {
+    JLayeredPane layeredPaneTopRight = new JLayeredPane();
+    layeredPaneTopRight.setBounds(pW/2, 0, pW/2 ,pH/2);
+    layeredPaneTopRight.setBorder(new LineBorder(Color.white));
+
+    return layeredPaneTopRight;
+  }
+
+  private static JLayeredPane createJLayeredPaneBottomLeft(int pW, int pH)
+  {
+    JLayeredPane layeredPaneBottomLeft = new JLayeredPane();
+    layeredPaneBottomLeft.setBounds(0, pH/2, pW/2, pH/2);
+    layeredPaneBottomLeft.setBorder(new LineBorder(Color.cyan));
+    
+    return layeredPaneBottomLeft;
+  }
+
+  private static JLayeredPane createJLayeredPaneBottomRight(int pW, int pH)
+  {
+    JLayeredPane layeredPaneBottomRight = new JLayeredPane();
+    layeredPaneBottomRight.setBounds(pW/2 ,pH/2,pW/2 ,pH/2);
+    layeredPaneBottomRight.setBorder(new LineBorder(Color.green));
+
+    return layeredPaneBottomRight;
+  }
 }
