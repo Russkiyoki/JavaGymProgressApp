b5df73b6f4d9b944753ef8ccb9220c397de9fc6a
Russkiyoki <elivoich@gmail.com>
2024-08-31 19:06:35 -0700

    DB ACCSSESSDED

---
+++ asd.sql
@@ -0,0 +1,7 @@
+SELECT * FROM person_info;
+-- we currently have 1 table called person_info
+-- and within it we have id, first_name, and last_name
+-- and within those we have 0 Elisey Voichishin,
+-- and 1 Austin Greenland
+
+-- *borat voice* great success
